491aa481e9470431107c918dbf6383d61bfd17d3
0KABE <RIN.OKAB3@GMAIL.COM>
2023-08-13 22:59:34 +0800

    Create a C++ project

---
+++ include/example.h
@@ -0,0 +1,14 @@
+//
+// Created by Chen, WenTao on 2023/8/12.
+//
+
+
+#pragma once
+
+namespace ExampleNamespace {
+
+class Example {
+
+};
+
+} // ExampleNamespace
---
+++ src/example.cc
@@ -0,0 +1,8 @@
+//
+// Created by Chen, WenTao on 2023/8/12.
+//
+
+#include "example.h"
+
+namespace ExampleNamespace {
+} // ExampleNamespace
